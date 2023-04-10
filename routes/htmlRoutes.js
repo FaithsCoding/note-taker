ae35a5a49f3079b3b30b06f60c7aab6cd19f1850
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-04-10 19:03:38 +0100

    added GET requests to fetch index.html & notes.html

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Dependencies
+const path = require('path');
+
+// Route
+module.exports = (app) => {
+  // GET request which returns notes.html
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  // GET request to return index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
