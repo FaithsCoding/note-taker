9f5b5053235a651cb4f1bb706e07e3eaa0f90253
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-04-10 13:33:36 +0100

    added a delete request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,44 @@
-//dependencies
+// Dependencies
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 
-//node package allowing id's to have unique names
-var uniqid = require('uniqid');
+// Node package allowing IDs to have unique names
+const uniqid = require('uniqid');
 
-//begining of the routing
+// Beginning of the routing
 module.exports = (app) => {
-    //GET request asking the api/notes read the db.json file and return saved notes as JSON
-    app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '..db/db.json'));
-    });
+  // GET request asking the api/notes to read the db.json file and return saved notes as JSON
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'db', 'db.json'));
+  });
 
-    //POST request asking the api/notes receives a new note to save which gets added to the db.json file and returns a new note for the user
-    app,post('/api/notes', (req, res) => {
-        let db = fs.readFileSync('db/db.json');
-        db = JSON.parse(db);
-        res.json(db);
-        
-        //body for the notes
-        let userNote = {
-            title: req.body.title,
-            text: req.body.text,
-            // using uniqid to create an id which is specified
-            id: uniqid(),
-        };
-        // PUSH the new note into the db.json file
-        db.push(userNote);
-        fs.writeFileSync('db/db.json', JSON.stringify(db));
-        res.json(db;);
-    });
-}
+  // POST request asking the api/notes to receive a new note to save which gets added to the db.json file and returns a new note for the user
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+
+    // Body for the notes
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      // Using uniqid to create an ID which is specified
+      id: uniqid(),
+    };
+
+    // PUSH the new note into the db.json file
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+  });
+
+  // DELETE request providing a parameter for /api/notes:id containing the ID of the note to delete
+  app.delete('/api/notes/:id', (req, res) => {
+    // This let reads the notes from the db.json
+    let db = JSON.parse(fs.readFileSync('db/db.json'));
+    // Picks out the ID and removes it
+    let deleteNotes = db.filter((item) => item.id !== req.params.id);
+    // Rewriting the note to db.json
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+  });
+};
