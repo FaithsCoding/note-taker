5fbc34921686e521e34eba34d92bb850fa0a8bef
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-04-10 13:10:06 +0100

    created a request to create a route for all files in public folder, added a handler, added required route files and an app listener

--- server.js
+++ server.js
@@ -2,3 +2,18 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//asks express to create a route for every file in the public folder and use the '/' route.
+app.use(express.static("public"));
+//sets up express app to handle the parsing data e.g middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//route files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//starts the server, also known as an app listener
+app.listen(PORT, () => {
+  console.log("your server is available at localhost ${PORT}");
+});
