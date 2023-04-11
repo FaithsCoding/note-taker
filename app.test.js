406f6ae5781669cb45abd999771477c37f07bbc4
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-04-11 14:17:49 +0100

    added a middleware file

---
+++ app.test.js
@@ -0,0 +1,64 @@
+const server = require("./server");
+const request = supertest(`http://localhost:${4000}`);
+const request = supertest(server);
+
+describe("Test the note taking app routes", () => {
+  // Test the GET /notes endpoint
+  it("should respond with status code 200 and return notes.html", async () => {
+    const response = await request(app).get("/notes");
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual("text/html");
+  });
+
+  // Test the GET /api/notes endpoint
+  it("should respond with status code 200 and return an array of notes", async () => {
+    const response = await request(app).get("/api/notes");
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual("application/json");
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  // Test the POST /api/notes endpoint
+  it("should respond with status code 200 and return the added note", async () => {
+    const note = {
+      title: "Test Note",
+      text: "This is a test note.",
+    };
+    const response = await request(app).post("/api/notes").send(note);
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual("application/json");
+    expect(response.body.title).toEqual(note.title);
+    expect(response.body.text).toEqual(note.text);
+  });
+
+  // Test the DELETE /api/notes/:id endpoint
+  it("should respond with status code 200 and return an array of notes without the deleted note", async () => {
+    const noteToDelete = {
+      title: "Test Note",
+      text: "This is a test note.",
+    };
+    const noteToAdd = {
+      title: "Another Test Note",
+      text: "This is another test note.",
+    };
+    // First add a note to the list
+    const addedNoteResponse = await request(app)
+      .post("/api/notes")
+      .send(noteToAdd);
+    const addedNote = addedNoteResponse.body;
+
+    // Then add a note to delete
+    const noteToDeleteResponse = await request(app)
+      .post("/api/notes")
+      .send(noteToDelete);
+    const noteToDeleteId = noteToDeleteResponse.body.id;
+
+    // Then delete the note
+    const response = await request(app).delete(`/api/notes/${noteToDeleteId}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toEqual("application/json");
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body).not.toContainEqual(noteToDelete);
+    expect(response.body).toContainEqual(addedNote);
+  });
+});
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 //dependencies
+const fs = require("fs");
 const express = require("express");
+const path = require("path");
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 //asks express to create a route for every file in the public folder and use the '/' route.
@@ -10,8 +14,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //route files
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//route to read db.json and return saved notes as JSON
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+//route to index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+//receieve a new note and add it to the db.json file and return the saved note
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteLength = noteList.length + 1;
+
+  //creates new id for the property basedd on length and assigns it to the object
+  newNote.id = noteLength;
+  //pushes new note
+  noteList.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
+});
+
+//deletes notes using their id
+app.delete("/api/notes/:id", (req, res) => {
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteId = req.params.id.toString();
+
+  //creates an array which filters by id and deletes accordingly.
+  noteList = noteList.filter((selected) => {
+    return selected.id != noteId;
+  });
+
+  //updates db.json and returns the updated note
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
+});
 
 //starts the server, also known as an app listener
 app.listen(PORT, () => {
